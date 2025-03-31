29416b9fe0b0d5b6c2d50a9ad3a596fd5f9fe80b
Meranic <meranic77@gmail.com>
2025-03-31 10:51:45 +0200

    Creation front

---
+++ rainbowdb/src/app/shared/chat/chat-widget/chat-widget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-widget',
+  imports: [],
+  templateUrl: './chat-widget.component.html',
+  styleUrl: './chat-widget.component.scss'
+})
+export class ChatWidgetComponent {
+
+}
